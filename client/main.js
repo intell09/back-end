448e6e7e17b6906ac3fa7b021443179f63a66161
intell09 <inteldadeveloper@gmail>
2022-10-18 21:37:03 -0400

    initial commit

--- client/main.js
+++ client/main.js
@@ -1,14 +1,30 @@
-const complimentBtn = document.getElementById("complimentButton")
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require('axios');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
-    
-};
+let pokemon = [];
+axios.get('https://pokeapi.co/api/v2/pokemon').then(({data: {results} }) => {
+    pokemon = results 
+})
 
+app.get('/api/RandomPokemon', (req, res) => {
+    const rando = pokemon[Math.floor(math.random() * pokemon.length)]
+    res.status(200).send('rando');
+})
 
-complimentBtn.addEventListener('click', getCompliment);
+app.get('./api/All-pokemon', (req, res) => {
+ res.status(200).send('pokemon');
+})
+
+app.delete('./api/pokemon/:id', (req, res) => {
+    const {id} = req.params; 
+    pokemonArr = pokemonArr.filter((pokemon) => pokemon.id !== +id);
+    res.status(200).send(pokemonArr);
+})
+
+app.listen(4000, ()=> console.log('listening on 4000'))
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,52 @@
-const express = require("express");
-const cors = require("cors");
+//axios.get('http://localhost:4000/api/RandomPokemon').then((res => {
+    //console.log(res)
+//}))
 
-const app = express();
+const res = require("express/lib/response")
 
-app.use(cors());
+const pokeButton = document.getElementById('get-pokemon')
+const pokeBall = document.getElementById('pokeball')
+const BASE_URL = 'http://localhost:4000/api/'
 
-app.use(express.json());
+const renderPokemon = ({ data }) => {
+        data.forEach((pokemon) => {
+                const pokemonContainer = document.createElement('div')
+                const pokemonImg= document.createElement('img')
+                const pokemonLabel = document.createElement('label')
+                pokemonContainer.addEventListener('click', () => battlePokemon (pokemon))
+                pokemonLabel.textContent = pokemon.name 
+                pokemonImg.src = pokemon.sprites.front_default 
+                pokemonContainer.append(pokemonImg, pokemonLabel)
+                pokeBall.appendChild(pokemonContainer);
+})
 
-const { getCompliment } = require('./controller')
+}
 
-app.get("/api/compliment", getCompliment);
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const contenders = [];
+
+const battlePokemon = (pokemon) => {
+ contenders.push(pokemon)
+ if(contenders.length === 1) return 
+ const [ contender1, contender2 ] = contenders 
+ const pokemon1BattleRoll = Math.random () * contender1.base_experience
+ const pokemon2BattleRoll = Math.random () * contender2.base_experience
+ if(pokemon1BattleRoll > pokemon2BattleRoll){
+     deletePokemon(contender1.id)
+ } else {
+     deletePokemon(contender2.id)
+ }
+ }
+}
+
+function deletePokemon (id) {
+    axios.delete(`${BASE_URL}/pokemon/${id}`).then((res) => {
+        pokeBall.innerHTML = ""
+        renderPokemon(res);
+})
+}
+
+pokeButton.addEventListener('click', () => {
+    axios.get(`${BASE_URL}/api/all-pokemon`).then(renderPokemon)     
+        
+})
